6e299e4fd6e6320c0602eb08a4d96d572efbdf9b
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-07-12 23:56:57 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define oo 1e9
+#define EPS 1e-9 // precisa?
+#define ii pair<int, int>
+#define ff first
+#define ss second
+// colocar o for do cristiano
+// colocar o DEBUG(x)
+// substituir pair por tupla?
+
+int main() {
+
+}
