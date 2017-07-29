e3167dd4b8875c03ee8aa7509ffa871b1a932959
Ikaguia <Ikaguia@users.noreply.github.com>
2017-07-29 20:35:17 -0300

    Update template.cpp
    added default main, FOR2 and fixed 'ii'

--- template.cpp
+++ template.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(cont,max) for(int (cont)=0;		(cont)<(int)(max);(cont)++)
-#define LOG(x)        (31 - __builtin_clz(x))
-#define DEBUG(x)      cout << #x << " = " << x << endl;
+#define FOR(cont,max)        for(int (cont) = 0,       _MAX = (max);(cont) < _MAX; (cont)++)
+#define FOR2(cont,start,max) for(int (cont) = (start), _MAX = (max);(cont) < _MAX; (cont)++)
+#define LOG(x)               (31 - __builtin_clz(x))
+#define DEBUG(x)             cout << #x << " = " << x << endl;
 
-using ii = pair<int, int>
+using ii = pair<int, int>;
 #define ff first
 #define ss second
 #define ll long long
 #define oo 1e9
-#define EPS 1e-9 // precisa?
+#define EPS 1e-9
 
-int main() {
+int casos,caso = 0;
 
+int main(){
+	int n;
+	scanf(" %d",&casos);
+	for(caso = 0;caso < casos;caso++){
+		scanf(" %d",&n);
+		FOR(i,n){
+		}
+	}
 }
