b29660b45ef878c0faf55d8810e2bc099c44ad22
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-08-16 20:55:12 -0300

    Update algorithms.cpp

--- algorithms.cpp
+++ algorithms.cpp
@@ -9,13 +9,13 @@ void djikstra(int src = 1) {
     dist.resize(g.size(), oo);
     priority_queue<ii, vector<ii>, greater<ii>> pq;
 
-    pq.pb(dist[src] = 0, src);
+    pq.pb({dist[src] = 0, src});
 
     while(not pq.empty()) {
         auto c = pq.top(); pq.pop();
         for(auto e : g[c.ss]) {
             if(dist[e.ss] > c.ff + e.ff) {
-                pq.pb(dist[e.ss] = c.ff + e.ff, e.ss);
+                pq.pb({dist[e.ss] = c.ff + e.ff, e.ss});
             }
         }
     }
