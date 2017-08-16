8b4bc2b0d7d2df9b29d7f7c9403bea1580e78302
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-08-16 20:15:28 -0300

    adding djikstra

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,4 +1,27 @@
 ///////////////
+// DJIKSTRA       -need_revision
+///////////////
+
+vector<int> dist;
+vector<vector<ii>> g; // u w v
+
+void djikstra(int src) {
+	dist.resize(g.size(), oo);
+	priority_queue<ii, vector<ii>, greater<ii>> pq;
+	
+	pq.pb(dist[src] = 0, src);
+	
+	while(not pq.empty()) {
+		auto c = pq.top(); pq.pop();
+		for(auto e : g[c.ss]) {
+			if(dist[e.ss] > c.ff + e.ff) {
+				pq.pb(dist[e.ss] = c.ff + e.ff, e.ss);
+			}
+		}
+	}
+}
+
+///////////////
 // FAST FIBONNACI       -need_revision
 ///////////////
 
