0f7503913c41defe0925da50a97411168947a28f
Ikaguia <cristianobrust123@gmail.com>
2017-08-26 08:30:27 -0300

    pb

--- template.hpp
+++ template.hpp
@@ -10,7 +10,7 @@ using namespace std;
 #define FOR(cont,max)        for(int (cont) = 0,       _MAX = (max);(cont) < _MAX; (cont)++)
 #define FOR2(cont,start,max) for(int (cont) = (start), _MAX = (max);(cont) < _MAX; (cont)++)
 #define LOG(x)               (31 - __builtin_clz(x))
-#define DEBUG(x)             cout << #x << " = " << x << endl;
+#define DEBUG(x)             cerr << #x << " = " << x << endl;
 
 using ii = pair<int, int>;
 #define ff first
@@ -18,6 +18,7 @@ using ii = pair<int, int>;
 #define ll long long
 #define oo 1e9
 #define EPS 1e-9
+#define pb push_back
 
 
 
