684f0c510dd36bbdc26a7f205754d442349e5283
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-08-26 10:33:09 -0300

    Update algorithms.cpp

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,30 +1,20 @@
-///////////////
-// DJIKSTRA       -need_revision
-///////////////
-
+// DJIKSTRA       -need_revision //
 vector<int> dist; // answer -> dist[destiny]
 vector<vector<ii>> g; // u w v
 
 void djikstra(int src = 1) { 
     dist.resize(g.size(), oo);
     priority_queue<ii, vector<ii>, greater<ii>> pq;
-
     pq.pb({dist[src] = 0, src});
-
     while(not pq.empty()) {
         auto c = pq.top(); pq.pop();
-        for(auto e : g[c.ss]) {
-            if(dist[e.ss] > c.ff + e.ff) {
+        for(auto e : g[c.ss])
+            if(dist[e.ss] > c.ff + e.ff)
                 pq.pb({dist[e.ss] = c.ff + e.ff, e.ss});
-            }
-        }
     }
 }
 
-///////////////
-// FAST FIBONNACI       -need_revision
-///////////////
-
+// FAST FIBONNACI       -need_revision //
 typedef struct { int v[2][2]; } matriz;
 matriz I, FIB;
 
@@ -42,32 +32,25 @@ matriz fastPot(matriz x, int exp){ // FASTPOT MATRIZ EDITION
     return fastPot(MM(x, x), exp/2);
 }
 
-void startFastPot(int N) {
+void startFastPot(int N) {  	// resposta está em FIB.v[1][0]
     I.v[0][1] = I.v[1][0] = 0; I.v[0][0] = I.v[1][1] = 1; // matriz identidade
     FIB.v[0][0] = FIB.v[0][1] = FIB.v[1][0] = 1; FIB.v[1][1] = 0;
     FIB = fastPot(INI, N); // N é o n-ézimo número de fibonacci
- 	// resposta está em FIB.v[1][0]
 }
 
-///////////////
-// Binary Search
-///////////////
-
+// Binary Search //
 int bb(int* v, int p, int q, int x){
 	int r;
 	while(p < q){
 		r = (p + q)/2;
 		if(v[r] == x) return r;
-		if(v[r] > x)	q = r-1;
-		else 			p = r+1;
+		if(v[r] > x) q = r-1;
+		else p = r+1;
 	}
 	return -1;
 }
 
-///////////////
-// Binomial Coefficient -need_revision
-///////////////
-
+// Binomial Coefficient -need_revision //
 long binomial_coefficient(n,m){ /* Calculo de Arranjo Rápido */
 	int n,m;
 	int i,j;
@@ -80,31 +63,16 @@ long binomial_coefficient(n,m){ /* Calculo de Arranjo Rápido */
 	return( bc[n][m] );
 }
 
-///////////////
-// GCD
-///////////////
-
+// GCD //
 int gcd(int a, int b){ return (a%b)? gcd(b, a%b) : b; }
 
-///////////////
-// LCM
-///////////////
-
+// LCM //
 int lcm(int a, int b){ auto g = gcd(a, b); return g ? (a / g * b) : 0; }
 
-///////////////
-// Josefus Problem
-///////////////
+// Josefus Problem //
+int f(int n_people, int jump){ return (n == 1)? 1 : (f(n - 1, k) + k - 1) % n + 1; }
 
-int f(int n, int k){	// Quantidade de pessoas e o tamanho do salto
-	if(n == 1) return 1;
-	return (f(n-1, k) + k - 1)%n + 1;
-}
-
-///////////////
-// Merge Sort
-///////////////
-
+// Merge Sort //
 void intercala( int * v, int p, int q, int r){
 	int i = p, j = q, k = 0;
 	vector<int> w(r-p);
@@ -128,32 +96,16 @@ void merge_sort (int* v, int p, int r){
 	}
 }
 
-///////////////
-// Bit Manipulation
-///////////////
+// Bit Manipulation //
+y  =  (x & (1 << i)) // Get the i-th bit
+x |=  (1 << i)       // Set the i-th bit
+x &= ~(1 << i)       // Clear the i-th bit
 
-y = (x & (1 << i)) // Get the i-th bit
-
-x |= (1 << i) // Set the i-th bit
-
-x &= ~(1 << i) // Clear the i-th bit
-
-///////////////
-// Tips
-///////////////
-
-// Permutações
+// Tips //
 next_permutation(myints,myints+3)
 prev_permutation(myints,myints+3)
 
-bool is_power_of_2(int n) {
-    if (n <= 0) {
-        return 0;
-    }
-    return !(n & (n - 1));
-}
+bool is_power_of_2(int n) { return (n <= 0)? 0 : !(n & (n - 1)); }
 
 scanf("%x"); // le como heaxadecimal
-
 scanf("%e"); // le como notação cientifica
-
