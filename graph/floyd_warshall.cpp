3c961d2bf724c1715e8e6e82edd89fb66109c7f7
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-12-26 17:45:48 -0200

    Update floyd_warshall.cpp

--- graph/floyd_warshall.cpp
+++ graph/floyd_warshall.cpp
@@ -1,7 +1,9 @@
 /* 
-	init: p[i][j] = i;
-	if (i,k)+(k,j) < (i,j)
-	 p[i][j] = p[k][j]
+	init: p[i][j] = i ; d[V][V];
+	for(0, V, i): for(0, V, j): for(0, V, k):
+		if d[i][k]+p[k][j] < d[i][j]:
+			p[i][j] = p[k][j]
+			d[i][j] = d[i][k] + d[k][j]
 */
          
          
