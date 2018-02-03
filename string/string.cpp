395a09bfc6fdacfc8aa3a8e3c5f1f8c9b60c5ac9
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-02-03 16:41:55 -0200

    Update string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -5,3 +5,4 @@ stoll(s, NULL, 2); // convert string in binary base to base 10
 to_string(999); // convert to string 
 s.substr(i, j); // get the substring of i to j
 transform(begin(s), end(s), begin(s), ::tolower); // to upper
+getline(is, str, '.'); // istringstream, string, char( ='\n')
