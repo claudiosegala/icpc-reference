104f3f7a600737b1ac559b9aa6316a85710d9731
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-02-03 13:39:47 -0200

    just some string functions

---
+++ string/string.cpp
@@ -0,0 +1,7 @@
+// Useful String Stuff
+stod(s); // convert to double
+stoi(s); // convert to int
+stoll(s, NULL, 2); // convert string in binary base to base 10 
+to_string(999); // convert to string 
+s.substr(i, j); // get the substring of i to j
+transform(begin(s), end(s), begin(s), ::tolower); // to upper
