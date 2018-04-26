5f2580fe836e7e62c23bc2cc0c44b239beff11df
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-04-26 15:34:25 -0300

    Update bit.cpp

--- math/bit.cpp
+++ math/bit.cpp
@@ -3,4 +3,4 @@ x |=  (1 << i)       // Set the i-th bit
 x &= ~(1 << i)       // Clear the i-th bit
 (x && !(x &(x-1)))   // if power of 2
 1 << (32 - __builtin_clz (n - 1)) // minimum power of 2 bigger than
- n&(-n)              // rightmost bit 
+n&(-n)              // rightmost bit 
