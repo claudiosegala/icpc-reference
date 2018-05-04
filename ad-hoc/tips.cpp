8a7c484474f1ad9822007f85b25e1cb4cdf19b07
claudiosegala <claudiosegalafilho@gmail.com>
2018-05-04 08:06:22 -0300

    added some geometry classes

--- ad-hoc/tips.cpp
+++ ad-hoc/tips.cpp
@@ -1,7 +1,4 @@
-next_permutation(myints,myints+3)
-prev_permutation(myints,myints+3)
-
-bool is_power_of_2(int n) { return (n <= 0)? 0 : !(n & (n - 1)); }
+next_permutation(myints,myints+3); // prev_
 
 scanf("%x"); // le como hexadecimal
 scanf("%e"); // le como notacao cientifica
