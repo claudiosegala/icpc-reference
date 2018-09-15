0a7e99e7f59a7611f97f92bfed9776a0c6e25a05
Ikaguia <Ikaguia@users.noreply.github.com>
2018-09-15 02:42:51 -0300

    Update gale_shapley.cpp

--- graph/gale_shapley.cpp
+++ graph/gale_shapley.cpp
@@ -1,11 +1,11 @@
 Assign each person to be free;
   	while (some man m is free) do
   	begin
-  		w := first woman on m s list;
+  		w := first woman on ms list;
   		m proposes, and becomes engaged, to w;
   		if (some man m' is engaged to w) then
   		    assign m' to be free;
-  		for each (successor m'' of m on w’s list) do
+  		for each (successor m'' of m on ws list) do
   			delete the pair (m'', w)
  	  end;
    	output the engaged pairs, which form a stable matching
