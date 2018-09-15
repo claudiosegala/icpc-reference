785a681e80b7ca3557aae0c40a070ba0143a19b9
Ikaguia <ikaguia@github.com>
2018-09-15 02:24:17 -0300

    changing template

--- config/template.cpp
+++ config/template.cpp
@@ -1,21 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FILL(X, V)           memset((X), (V), sizeof(X))
-#define SIZE(V)              int((V).size())
-#define FOR2(c,i,j) for(int(c)=(i),_MAX=(j);(c)<_MAX;(c)++)
-#define FOR(cont,max)        FOR2((cont), 0, (max))
-#define LOG(x)               (31 - __builtin_clz(x))
-#define W(x)                 cerr << "\033[31m" << #x << "=" << x << "\033[0m" << "\n";
-#define ii 					 pair<int, int>
-#define ff 					 first
-#define ss 					 second
-#define oo 					 1e9
-#define ep 					 1e-9
-#define pb 					 push_back
+#define FOR(cont,start,end)	for(int (cont) = (start), cont##_END = (end);   (cont) <  cont##_END; (cont)++)
+#define ROF(cont,start,end)	for(int (cont) = (end)-1, cont##_END = (start); (cont) >= cont##_END; (cont)--)
 
-typedef long long ll;
-typedef unsigned long long ul;
+#ifdef ONLINE_JUDGE
+#define W(x)				cerr << "\033[31m" << #x << "=" << x << "\033[0m" << "\n";
+#else
+#define W(x)
+#endif
+
+#define FILL(X, V)			memset((X), (V), sizeof(X))
+#define SIZE(V)				uint((V).size())
+#define LOG(x)				(31 - __builtin_clz(x))
+
+#define ff 					first
+#define ss 					second
+#define pb 					emplace_back
+
+using ull = unsigned long long;
+using ll = long long;
+using ii = pair<int,int>;
+
+const ll oo = 1e9;
+const ll MOD = 1e9+7;
+const double ep = 1e-9;
+
 
 int main() {
 	ios::sync_with_stdio(false);
