d45d8416a36b251b4b5f739bb03021cb703fb162
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-09-16 07:28:23 -0300

    refactoring on the template

--- config/template.cpp
+++ config/template.cpp
@@ -1,22 +1,23 @@
+// Use g++ -std=c++14 -static -O2 -Wall -g -DDEBUG "name".cpp
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define FOR(cont,start,end)	for(int (cont) = (start), cont##_END = (end);   (cont) <  cont##_END; (cont)++)
-#define ROF(cont,start,end)	for(int (cont) = (end)-1, cont##_END = (start); (cont) >= cont##_END; (cont)--)
-
-#ifdef ONLINE_JUDGE
-#define W(x)				cerr << "\033[31m" << #x << "=" << x << "\033[0m" << "\n";
+#ifdef DEBUG
+#define W(x)         cerr << "\033[31m" << #x << "=" << x << "\033[0m" << "\n";
 #else
 #define W(x)
 #endif
 
-#define FILL(X, V)			memset((X), (V), sizeof(X))
-#define SIZE(V)				uint((V).size())
-#define LOG(x)				(31 - __builtin_clz(x))
-
-#define ff 					first
-#define ss 					second
-#define pb 					emplace_back
+#define FOR(cnt,I,E) for(int (cnt) = (I), cnt##_END = (E);   (cnt) <  cnt##_END; (cnt)++)
+#define ROF(cnt,I,E) for(int (cnt) = (E)-1, cnt##_END = (I); (cnt) >= cnt##_END; (cnt)--)
+#define FILL(X, V)   memset((X), (V), sizeof(X))
+#define SIZE(V)      uint((V).size())
+#define LOG(x)       (31 - __builtin_clz(x))
+#define ff           first
+#define ss           second
+#define pb           emplace_back
 
 using ull = unsigned long long;
 using ll = long long;
@@ -26,7 +27,8 @@ const ll oo = 1e9;
 const ll MOD = 1e9+7;
 const double ep = 1e-9;
 
-
 int main() {
-	ios::sync_with_stdio(false);
+	//ios::sync_with_stdio(false);
+
+	return 0;
 }
