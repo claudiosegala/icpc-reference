9d331ae5b91fc918de39a0bb89575c3c51a00522
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-18 08:57:49 -0300

    Adding a second version of josephus problem

--- ad-hoc/josephus.cpp
+++ ad-hoc/josephus.cpp
@@ -1,3 +1,10 @@
 int f(int n, int k){	// #people, jump size
 	return (n == 1) 1 : (f(n-1, k) + k - 1) % n + 1;
 }
+//--------
+int live[MAXN];
+void josephus( int n, int m ){ // n people, m-th get killed
+	live[1] = 0;
+	for( int i = 2; i <= n; i++ )
+		live[i] = (live[i-1]+(m%i))%i;
+}
