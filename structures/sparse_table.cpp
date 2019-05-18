247972c4f1eb5c8aa6773813691aa9122382cee9
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-18 10:51:52 -0300

    Adding sparse table

---
+++ structures/sparse_table.cpp
@@ -0,0 +1,10 @@
+int fn(int i, int j){
+	if(j == 0) return v[i];
+	if(~dn[i][j]) return dn[i][j];
+	return dn[i][j] = min(fn(i, j-1), fn(i + (1 << (j-1)), j-1));
+}
+
+int getmn(int l, int r){ // [l, r]
+	int lz = lg(r - l + 1); //  maximum p that 2^p <= i
+	return min(fn(l, lz), fn(r - (1 << lz) + 1, lz));
+}
