73763b51cd687b7638be4684096f09569a079d92
claudiosegala <claudiosegalafilho@gmail.com>
2019-05-18 10:09:25 -0300

    adding python stuff

---
+++ tricks/python.py
@@ -0,0 +1,33 @@
+# read "1 2 3 4 5"
+v = map(int, raw_input().split())
+
+# swap
+a, b = b, a
+
+# array start at 1
+# X = [1, 2, 3]
+X[:2] # [1, 2], all under idx 2
+X[2:] # [], all upper idx 2
+
+# list
+l = []
+l.append(k)
+l.sort()
+l.pop(idx)
+l.remove(item)
+l.reverse()
+
+# string 
+s = "asd"
+s.replace("a", "b")
+
+# map
+d = {'a': 1, 'b': 2}
+d.clear()
+for k,v in d.iteritems():
+    print k
+    print v
+
+# set
+_s = set(list)
+x is in _s # True or False
